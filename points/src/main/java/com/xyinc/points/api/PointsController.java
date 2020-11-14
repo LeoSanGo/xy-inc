b64b8af9cfccd70c112fd259186d78ccae0fa476
LeoSanGo <souoleo@gmail.com>
2020-11-14 15:47:43 -0300

    add method save object

--- points/src/main/java/com/xyinc/points/api/PointsController.java
+++ points/src/main/java/com/xyinc/points/api/PointsController.java
@@ -1,12 +1,19 @@
 package com.xyinc.points.api;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.xyinc.points.api.domain.Poi;
 import com.xyinc.points.api.domain.PoiService;
+import com.xyinc.points.api.domain.dto.PoiDTO;
 
 @RestController
 @RequestMapping("/points")
@@ -18,6 +25,24 @@ public class PointsController {
 	@GetMapping()
 	public ResponseEntity get() {
 		return ResponseEntity.ok(service.getPois()); //Verificar o retorno do Json
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity post(@RequestBody Poi poi) {
+		
+		try {
+			PoiDTO p =  service.insert(poi);
+			
+			URI location = getUri(p.getId());	
+			return ResponseEntity.created(location).build();
+		} catch (Exception ex) {
+			return ResponseEntity.badRequest().build();
+		}	
+	}
+	
+	private URI getUri(Long id) {
+		return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(id).toUri();
 	}
 	
 	
--- points/src/main/java/com/xyinc/points/api/domain/PoiService.java
+++ points/src/main/java/com/xyinc/points/api/domain/PoiService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import com.xyinc.points.api.domain.dto.PoiDTO;
 @Service
@@ -15,6 +16,12 @@ public class PoiService {
 	
 	public List<PoiDTO> getPois() {
 		return rep.findAll().stream().map(PoiDTO::create).collect(Collectors.toList());
+	}
+	
+	public PoiDTO insert(Poi poi) {
+		Assert.isNull(poi.getId(), "Não foi possivel inserir o registro");
+		return PoiDTO.create(rep.save(poi));
+		
 	}
 
 }
